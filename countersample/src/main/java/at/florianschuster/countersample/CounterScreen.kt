ef13e4be11655d4b06b620fd58e9533d82035e5e
zhuinden <zhuinden@gmail.com>
2019-01-23 15:01:12 +0100

    Properly save / restore state in CounterExample

--- countersample/src/main/java/at/florianschuster/countersample/CounterScreen.kt
+++ countersample/src/main/java/at/florianschuster/countersample/CounterScreen.kt
@@ -1,14 +1,22 @@
 package at.florianschuster.countersample
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
 import at.florianschuster.androidreactor.ReactorView
 import at.florianschuster.androidreactor.ViewModelReactor
-import at.florianschuster.androidreactor.reactor
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.view.visibility
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parceler
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_counter.*
 import java.util.concurrent.TimeUnit
 
@@ -16,13 +24,35 @@ import java.util.concurrent.TimeUnit
 private const val layout = R.layout.activity_counter
 
 class CounterActivity : AppCompatActivity(), ReactorView<CounterReactor> {
-    override val reactor: CounterReactor by reactor()
+    private var stateSnapshot: CounterReactor.State = CounterReactor.State()
+
     override val disposables = CompositeDisposable()
+
+    @Suppress("UNCHECKED_CAST")
+    override val reactor: CounterReactor by lazy {
+        ViewModelProviders.of(this@CounterActivity, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                if (modelClass == CounterReactor::class.java) {
+                    return CounterReactor(stateSnapshot) as T
+                }
+                throw IllegalArgumentException("Cannot create ${modelClass.name}")
+            }
+        }).get(CounterReactor::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            stateSnapshot = savedInstanceState.getParcelable("stateSnapshot")!!
+        }
+
         setContentView(layout)
         bind(reactor)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("stateSnapshot", stateSnapshot)
     }
 
     override fun onDestroy() {
@@ -51,11 +81,15 @@ class CounterActivity : AppCompatActivity(), ReactorView<CounterReactor> {
             .distinctUntilChanged()
             .subscribe(progressLoading.visibility())
             .let(disposables::add)
+
+        reactor.state
+            .subscribe { state -> stateSnapshot = state }
+            .let(disposables::add)
     }
 }
 
 
-class CounterReactor : ViewModelReactor<CounterReactor.Action, CounterReactor.Mutation, CounterReactor.State>(State()) {
+class CounterReactor(initialState: State) : ViewModelReactor<CounterReactor.Action, CounterReactor.Mutation, CounterReactor.State>(initialState) {
     sealed class Action {
         object Increase : Action()
         object Decrease : Action()
@@ -67,10 +101,19 @@ class CounterReactor : ViewModelReactor<CounterReactor.Action, CounterReactor.Mu
         data class SetLoading(val loading: Boolean) : Mutation()
     }
 
+    @Parcelize
     data class State(
         val value: Int = 0,
-        val loading: Boolean = false
-    )
+        @Transient val loading: Boolean = false
+    ) : Parcelable {
+        companion object: Parceler<State> { // the only way to ignore a property with @Parcelize is to manually not save it.
+            override fun create(parcel: Parcel): State = State(parcel.readInt())
+
+            override fun State.write(parcel: Parcel, flags: Int) {
+                parcel.writeInt(value)
+            }
+        }
+    }
 
     override fun mutate(action: Action): Observable<out Mutation> = when (action) {
         is Action.Increase -> Observable.concat(
